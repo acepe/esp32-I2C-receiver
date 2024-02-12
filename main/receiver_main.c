a6aa8a12d3fe3cad63364bbb0e6ac5c1fd915fb5
Andre Ackermann <andre.a@gmx.de>
2024-02-12 11:27:27 +0100

    receiving in task

--- main/receiver_main.c
+++ main/receiver_main.c
@@ -1,54 +1,58 @@
 #include <stdio.h>
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
-#include "esp_log.h"
 #include "driver/i2c.h"
+#include "esp_log.h"
 
-static const char *TAG = "i2c_slave";
+#define I2C_SLAVE_SCL_IO     2                /*!< gpio number for I2C slave clock */
+#define I2C_SLAVE_SDA_IO     1                /*!< gpio number for I2C slave data  */
+#define I2C_SLAVE_ADDR       0x27             /*!< I2C slave address for this example */
+#define I2C_SLAVE_NUM       I2C_NUM_0        /*!< I2C port number for master dev */
+#define I2C_SLAVE_RX_BUF_LEN  1024             /*!< I2C slave buffer size for receive data */
+#define I2C_SLAVE_TX_BUF_LEN  1024             /*!< I2C slave buffer size for send data */
 
-#define I2C_SLAVE_SCL_IO           2        /*!< GPIO number for I2C slave clock  */
-#define I2C_SLAVE_SDA_IO           1        /*!< GPIO number for I2C slave data   */
-#define I2C_SLAVE_NUM              I2C_NUM_0 /*!< I2C slave port number for slave dev */
-#define I2C_SLAVE_ADDR             0x27      /*!< I2C slave address for the slave device */
-#define I2C_SLAVE_RX_BUF_LEN       1024      /*!< I2C slave buffer size */
+static const char *TAG = "I2C_SLAVE";
 
-uint8_t i2c_slave_rx_buffer[I2C_SLAVE_RX_BUF_LEN] = {0};
+/**
+ * @brief Task to process I2C slave events
+ */
+static void i2c_slave_task(void *arg) {
+    ESP_LOGI(TAG, "Task started");
 
-void i2c_slave_init() {
-  i2c_config_t conf;
-  conf.mode = I2C_MODE_SLAVE;
-  conf.sda_io_num = I2C_SLAVE_SDA_IO;
-  conf.sda_pullup_en = GPIO_PULLUP_ENABLE;
-  conf.scl_io_num = I2C_SLAVE_SCL_IO;
-  conf.scl_pullup_en = GPIO_PULLUP_ENABLE;
-  conf.slave.addr_10bit_en = 0;
-  conf.slave.slave_addr = I2C_SLAVE_ADDR;
-  conf.clk_flags = 0;
+    uint8_t *data = (uint8_t *)malloc(I2C_SLAVE_RX_BUF_LEN);
+    if (data == NULL) {
+        ESP_LOGE(TAG, "Failed to allocate memory for receive buffer");
+        vTaskDelete(NULL);
+    }
 
-  i2c_param_config(I2C_SLAVE_NUM, &conf);
-  i2c_driver_install(I2C_SLAVE_NUM, conf.mode, I2C_SLAVE_RX_BUF_LEN, 0, 0);
-  i2c_reset_rx_fifo(I2C_SLAVE_NUM);
+    int size = 0;
+    while (1) {      
+        size = i2c_slave_read_buffer(I2C_SLAVE_NUM, data, I2C_SLAVE_RX_BUF_LEN, 10 / portTICK_PERIOD_MS);
+        if (size > 0) {
+            // ESP_LOGI(TAG, "Received data:");
+            ESP_LOG_BUFFER_CHAR(TAG, data, size);
+        }
+    }
+
+    free(data);
+    vTaskDelete(NULL);
 }
-
-void i2c_slave_receive_hello() {
-  int len = i2c_slave_read_buffer(
-      I2C_SLAVE_NUM, i2c_slave_rx_buffer, sizeof(i2c_slave_rx_buffer),
-      500 / portTICK_PERIOD_MS);  // Use a timeout of 1000 ms
-  
-  if (len > 0) {
-    ESP_LOGI(TAG, "Received message from master: %.*s", len,
-             i2c_slave_rx_buffer);
-  } else {
-    ESP_LOGI(TAG, "No message received from master.");
-  }
-}
-
-
 
 void app_main() {
-    ESP_LOGI(TAG, "I2C Slave Task Started");
-    i2c_slave_init();
-    while (1) {
-        i2c_slave_receive_hello();
-    }
+    i2c_config_t conf = {
+        .mode = I2C_MODE_SLAVE,
+        .sda_io_num = I2C_SLAVE_SDA_IO,
+        .sda_pullup_en = GPIO_PULLUP_ENABLE,
+        .scl_io_num = I2C_SLAVE_SCL_IO,
+        .scl_pullup_en = GPIO_PULLUP_ENABLE,
+        .slave = {
+            .addr_10bit_en = 0,
+            .slave_addr = I2C_SLAVE_ADDR
+        }
+    };
+
+    ESP_ERROR_CHECK(i2c_param_config(I2C_SLAVE_NUM, &conf));
+    ESP_ERROR_CHECK(i2c_driver_install(I2C_SLAVE_NUM, conf.mode, I2C_SLAVE_RX_BUF_LEN, I2C_SLAVE_TX_BUF_LEN, 0));
+
+    xTaskCreate(i2c_slave_task, "i2c_slave_task", 4096, NULL, 10, NULL);
 }
